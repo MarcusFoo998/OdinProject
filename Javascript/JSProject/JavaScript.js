94abe2f6602e61a618b5df96c5037a8c8fdbcff0
Marcus <foo.marcus.personal@gmail.com>
2022-12-04 22:47:27 +0800

    New JS Project and Exercises

---
+++ Javascript/JSProject/JavaScript.js
@@ -0,0 +1,77 @@
+
+
+let flag = 0;
+
+//Gets a random element from the objects array;
+function getComputerChoice() {
+    const objects = ["Rock", "Paper", "Scissors"];
+    
+    var randomItem = objects[Math.floor(Math.random() * objects.length)];
+
+   return(randomItem);
+};
+
+function play(userInput, computerSelection) {
+
+    let userGuess=userInput.toLowerCase();
+    let compGuess=computerSelection.toLowerCase();
+
+    if((userGuess=="scissors" && compGuess == "paper") || (userGuess == "rock" && compGuess == "scissors") || (userGuess == "paper" && compGuess == "rock")) {
+        flag = 1;
+        return "Congratulations! You win! " + compGuess;
+    } else if ((userGuess=="scissors" && compGuess == "rock") || (userGuess == "rock" && compGuess == "paper") || (userGuess == "paper" && compGuess == "scissors")) {
+        flag = 2;
+        return "Too bad! You've lost! " + compGuess;
+    } else {
+        flag = 0;
+        return "Darn, it's a draw! Try again! " + compGuess;
+    };
+};
+
+function game(userInput) {
+    let playerSelection = "rock";
+    playerSelection = userInput;
+    let computerSelection = getComputerChoice();
+    let user = 0;
+    let computer = 0;
+
+    console.log(play(playerSelection, computerSelection));
+        if(flag === 1){
+            user++;
+            return ("Congrats! You're at " + user + "! The computer is at " + computer);
+        } else if(flag === 2) {
+            computer++;
+            return ("Oh no.. The computer is at " + computer + "! You're at " + user);
+        } else {
+            return "Don't worry, nothing happened.";
+        }
+
+}
+
+function game() {
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice();
+    let user = 0;
+    let computer = 0;
+
+    console.log(play(playerSelection, computerSelection));
+        if(flag === 1){
+            user++;
+            return ("Congrats! You're at " + user + "! The computer is at " + computer);
+        } else if(flag === 2) {
+            computer++;
+            return ("Oh no.. The computer is at " + computer + "! You're at " + user);
+        } else {
+            return "Don't worry, nothing happened.";
+        }
+
+}
+
+function fiveGames() {
+    for(let i=0; i<5; i++){
+        game();
+    }
+    
+}
+
+
